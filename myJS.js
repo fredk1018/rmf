8197cf844c8f1c091a8beb4b7ab439bb5e73c249
Fredrick F Khoury <ffkhoury@crimson.ua.edu>
2014-12-30 02:16:06 -0600

    Starter with node express and jquery

--- myJS.js
+++ myJS.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
 	
 
-function(){
-
-
-
-var x = '5';
-var n = '1213123132';
-var ww = 'asdasd';
-var wwss = 'asdasd';
-var thzz = "commit 6"
-}
+	alert('lets begin');
 
 
 
