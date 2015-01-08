ebb18abddd9593b7a64686d1f594724921471514
Fredrick Khoury <fredk1018@gmail.com>
2015-01-08 14:39:14 -0500

    second commit added map-canvas div in index added map.js to js fodler and put a map in there

---
+++ public/js/map.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	alert('Hey this is an alert from begginging of  map.js');
+
+
+
+	  function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+      alert('Hey this is an alert from end of  map.js');
+});
--- public/js/myJS.js
+++ public/js/myJS.js
@@ -1,30 +1,6 @@
 $(document).ready(function(){
 //discount click handler
-	$(".tour").on('click','button',function(){
-		var tour = $(this).closest('.tour');
-		var discount = tour.data('discount');
-		var price = $('<span>From $'+discount+'</span>')
-		tour.append(price);
-		$(this).remove();
-	});
-//filter handlers
-	//click handler for on-sale items
-	$('#filters').on('click', '.on-sale', function(){
-		$('.tour').removeClass('highlight');
-		$('.tour').filter(".on-sale").addClass('highlight');
 
-	});
-	//click handler for featured items
-	$('#filters').on('click', '.featured', function(){
-		$('.tour').removeClass('highlight');
-		$('.tour').filter(".featured").addClass('highlight');
-		
-	});
-	//click handler for filter buttons
-	$('#filters').on('click','button',function(){
-		$('button').removeClass('highlight');
-		$(this).addClass('highlight');
-	});
 
 
 
