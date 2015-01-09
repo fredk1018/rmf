8e00fc468335e65c8d62ccecb0b406e937c5b8d9
Fredrick Khoury <fredk1018@gmail.com>
2015-01-09 11:43:39 -0600

    jsfiddle link removed one line for simpler testing in map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,14 @@
 
-function drawMap(){
+// THIS I UR FIDDLE NUm POOP http://jsfiddle.net/ot0r1ukh/1/
 var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(33.439346,-86.88312500000001)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+var initialize = function(map) {
+	var mapOptions = {
+		zoom: 15,
+    	center: new google.maps.LatLng(33.439346,-86.88312500000001)
+  	};
+  	map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
 
 
   var trail1Co = [
@@ -24,46 +25,19 @@ function initialize() {
     strokeWeight: 2
   });
 
-  var trail2Co = [
-    new google.maps.LatLng(33.439, -86.8834),
-    new google.maps.LatLng(33.437, -86.8834),
-    new google.maps.LatLng(33.435, -86.8834),
-    new google.maps.LatLng(33.433, -86.8834)
-  ];
-  var trail2 = new google.maps.Polyline({
-    path: trail2Co,
-    geodesic: true,
-    strokeColor: '#2DA1D3',
-    strokeOpacity: 1.0,
-    strokeWeight: 2
-  });
-
-function addT1() {
-  trail1.setMap(map);
-}
-
-function removeT1() {
-  trail1.setMap(null);
-}
-
-
 
   trail1.setMap(map);
-  trail2.setMap(map);
-  removeT1();
+ 
+  
   
 }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-};
 
 
 $(document).ready(function(){
-	drawMap();
+	google.maps.event.addDomListener(window, 'load', initialize);
+	alert('shouldaloaded');
 	//click handler for filter buttons
 	$('#filters').on('click','.hideT',function(){
 		console.log('remove line');
