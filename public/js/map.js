0e755da5f5572dcdab2bfad2e3f88268f25c0bfb
Fredrick Khoury <fredk1018@gmail.com>
2015-01-09 00:07:04 -0500

    removed map from index.html into map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,11 @@
-$(document).ready(function(){
-	alert('Hey this is an alert from begginging of  map.js');
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
 
-
-                                                                  
-	alert('Hey this is an alert from end of  map.js');
-});
+google.maps.event.addDomListener(window, 'load', initialize);
