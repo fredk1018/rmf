690770df5134747849db9a5a9df92c0fd3d18006
Fredrick Khoury <fredk1018@gmail.com>
2015-01-08 20:15:08 -0500

    moved map from map.js to index.html

--- public/js/map.js
+++ public/js/map.js
@@ -2,22 +2,6 @@ $(document).ready(function(){
 	alert('Hey this is an alert from begginging of  map.js');
 
 
-function initialize() {
-  var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
-  var map=new google.maps.Map(document.getElementById("map-canvas"),mapProp);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-	
-
-
-
-
-
-
+                                                                  
 	alert('Hey this is an alert from end of  map.js');
 });
