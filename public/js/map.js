bcc1dbb88185c02e1cb08f1c08ab941b75ab22af
Fredrick Khoury <fredk1018@gmail.com>
2015-01-08 23:47:24 -0600

    added bootstraped added som stlinh make it looks bettter changed mid lat and long and zoom

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,12 @@
 var map;
+
+
+
+
 function initialize() {
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 15,
+    center: new google.maps.LatLng(33.439346,-86.88312500000001)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
